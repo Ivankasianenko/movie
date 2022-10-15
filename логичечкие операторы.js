b5a6f156b4e6e801fc4345912a93f86b0f109c70
Ivan <vanya.kasyanenko98@gmail.com>
2022-10-15 20:56:39 +0300

    LO

--- логичечкие операторы.js
+++ логичечкие операторы.js
@@ -10,8 +10,46 @@
 
 const hamburger = 2;
 const fries = 1; 
-if (hamburger === 3&& fries) {
+if (hamburger === 3 && fries) {
  console.log('Все сыты!');
 } else{
     console.log('Мы уходим');
 }
+
+console.log( NaN || 2 || undefined );  
+
+console.log( NaN && 2 && undefined ); 
+
+
+console.log( 1 && 2 && 3 ); 
+
+
+console.log( !1 && 2 || !3 ); 
+
+console.log( 25 || null && !3 ); 
+
+console.log( NaN || null || !3 || undefined || 5); 
+
+
+console.log( NaN || null && !3 && undefined || 5);
+
+
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!')
+
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!')
+}
+
